55ff0fe22a757a9d39fe50f4be78a267d8a67ad4
inkarnadin <nasyrovvad@gmail.ru>
2021-03-21 01:25:20 +0300

    test: fix

--- src/test/java/scanner/cve/ConfigurationDecryptTest.java
+++ src/test/java/scanner/cve/ConfigurationDecryptTest.java
@@ -8,7 +8,7 @@ public class ConfigurationDecryptTest {
     @Test
     public void parse() {
         String parse = ConfigurationDecrypt.parse("\u0000admin\u0000\u0001\u0002#test*\u0000\u0000\u0001\u0002");
-        Assert.assertEquals("[admin:#test*]", parse);
+        Assert.assertEquals("admin:#test*", parse);
     }
 
 }
