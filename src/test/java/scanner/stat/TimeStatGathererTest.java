fc79d51c419f72fad6c18d6bb4afa30feccfea1d
inkarnadin <nasyrovvad@gmail.com>
2021-10-05 17:52:06 +0300

    test: add test for duration calc

---
+++ src/test/java/scanner/stat/TimeStatGathererTest.java
@@ -0,0 +1,18 @@
+package scanner.stat;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static scanner.stat.TimeStatItem.TOTAL_TIME;
+
+public class TimeStatGathererTest {
+
+    @Test
+    public void testGetData() {
+        TimeStatGatherer gatherer = new TimeStatGatherer();
+        gatherer.set(TOTAL_TIME, 5000000005L);
+
+        Assert.assertEquals("1388:53:20.005", gatherer.getData().get(TOTAL_TIME.toString()));
+    }
+
+}
